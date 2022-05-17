1665a87856ab7e109108f3496f2f1be187a873a8
Ana Beatriz Martins Lima <ana.martins1912@gmail.com>
2022-05-17 15:29:32 -0300

    modificacao scripts

--- Fundamentos/bloco4_javascript/4.3/scripts.js
+++ Fundamentos/bloco4_javascript/4.3/scripts.js
@@ -1,5 +1,5 @@
 //exercise.js
-let fruits = [3, 4, 10, 1, 12];
+/*let fruits = [3, 4, 10, 1, 12];
 let sum = 0;
 
 for (let index = 0; index < fruits.length; index += 1) {
@@ -10,4 +10,60 @@ if (sum > 15) {
   console.log(sum);
 } else {
   console.log('valor menor que 16');
+}*/
+
+//exer 1
+/*let fatorial = 1;
+
+for (let i = 10; i > 0; i-= 1) {
+  fatorial *= i;
 }
+
+console.log(fatorial);*/
+
+/*let word = 'tryber';
+let reverseWord = '';
+ 
+for ( let i = 5; i>=0; i -=1){
+  reverseWord += word[i]
+  
+  } 
+  console.log(reverseWord);*/
+
+ /*let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);*/
+
+
+/*let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);*/
+
