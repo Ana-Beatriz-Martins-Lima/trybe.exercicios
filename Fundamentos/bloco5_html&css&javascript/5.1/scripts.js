6087961e25671a47730dcce7e4b0f7f061be9b23
Ana Beatriz Martins Lima <ana.martins1912@gmail.com>
2022-05-19 15:32:03 -0300

    exercicio 5.1

---
+++ Fundamentos/bloco5_html&css&javascript/5.1/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("page-title").innerText = "Um amor para Recordar"
+
+document.getElementById("second-paragraph").innerText = 
+"Um amor para recordar fala sobre um casal no qual a menina que tem leucemia, por tanto, pouco tempo de vida"
+
+document.getElementById("subtitle").innerText = " Uma linda história de amor"
