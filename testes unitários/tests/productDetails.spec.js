855346963cb9d960605d911c4876755293e53e1d
Ana Beatriz Martins Lima <ana.martins1912@gmail.com>
2022-07-07 20:14:46 -0300

    projeto testes unitarios

---
+++ testes unitários/tests/productDetails.spec.js
@@ -0,0 +1,50 @@
+const productDetails = require('../src/productDetails');
+/*
+  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara')
+
+  // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  Escreva pelo menos cinco testes para essa função para garantir que a implementação de productDetails está correta.
+
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste se productDetails é uma função.
+    expect(typeof productDetails).toEqual('function');
+    // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('carro','bola'))).toBeTruthy();
+    // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('carro','bola')).toHaveLength(2);
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('carro','bola')[0]).toEqual('object');
+    expect(typeof productDetails('carro','bola')[1]).toEqual('object');
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toBe(productDetails('Alcool gel', 'Máscara')[1])
+    // Teste se os dois productIds terminam com 123.
+    
+  });
+});
